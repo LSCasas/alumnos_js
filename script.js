d5c9b9be0c51bf7b6872c14df2dc3f045870a1c3
Luis Casas <luisscasas45@gmail.com>
2025-05-25 12:50:21 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+fetch("public/alumnos_100.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("No se pudo cargar el archivo JSON");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    const tbody = document.querySelector("#tabla-alumnos tbody");
+
+    data.forEach((alumno) => {
+      const fila = document.createElement("tr");
+      fila.innerHTML = `
+        <td>${alumno.matricula}</td>
+        <td>${alumno.Paterno}</td>
+        <td>${alumno.Materno}</td>
+        <td>${alumno.Nombre}</td>
+      `;
+      tbody.appendChild(fila);
+    });
+  })
+  .catch((error) => {
+    console.error("Error al cargar los alumnos:", error);
+  });
